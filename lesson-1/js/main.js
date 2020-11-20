d8d5857a7d41841035afe409a256c9ed74c61a3e
Evgeny <salabaikin@mail.ru>
2020-11-20 12:39:26 +0300

    lesson 1

---
+++ lesson-1/js/main.js
@@ -0,0 +1,8 @@
+// Создаем переменные
+let money, income, addExpenses, deposit, mission, period = 25;
+
+// Выводим всплывающее окно с сообщением
+alert('Lesson 1');
+
+// Выводим в консоль браузера сообщение
+console.log(`Message - ${period}`);
