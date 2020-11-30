eed44cb42e87b035371fde2440ba4d0bf0ebca44
Evgeny <salabaikin@mail.ru>
2020-11-30 14:44:05 +0300

    Lesson 7

--- lesson-7/js/main.js
+++ lesson-7/js/main.js
@@ -30,31 +30,35 @@ let appData = {
             let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
             appData.addExpenses = addExpenses.toLowerCase().split(',');
             appData.deposit = confirm('Есть ли у вас депозит в банке?');
+
+            let question, answer;
+
+            for(let i = 0; i < 2; i++) {
+                question = prompt('Введите обязательную статью расходов?');
+                do {
+                    answer = prompt('Во сколько это обойдется?');
+                } while (!isNumber(answer)) 
+                appData.expenses[question] = +answer;
+            }
         },
         getExpensesMonth: function() {
-            let sum = 0, amount = 0;
-            for(let i = 0; i < 2; i++) {
-                appData.expenses[i] = prompt('Введите обязательную статью расходов?');
-                do {
-                    amount = prompt('Во сколько это обойдется?');
-                } while (!isNumber(amount)) 
-        
-                sum += +amount;
+            for (let key in appData.expenses) {
+                appData.expensesMonth += appData.expenses[key];
             }
-            return sum;
         },
-        getAccumulatedMonth: function(money, expenses) {
-            return money - expenses;
+        getBudget: function() {
+            appData.budgetMonth = appData.budget - appData.expensesMonth;
+            appData.budgetDay = Math.floor(appData.budgetMonth / 30);
         },
         getTargetMonth: function() {
-            return Math.ceil(appData.mission/accumulatedMonth);
+            return Math.ceil(appData.mission/appData.budgetMonth);
         },
         getStatusIncome: function() {
-            if(budgetDay >= 1200) {
+            if(appData.budgetDay >= 1200) {
                 return console.log('У вас высокий уровень дохода');
-            } else if (budgetDay < 1200 && budgetDay >= 600) {
+            } else if (appData.budgetDay < 1200 && appData.budgetDay >= 600) {
                 return console.log('У вас средний уровень дохода');
-            } else if (budgetDay < 600 && budgetDay >= 0) {
+            } else if (appData.budgetDay < 600 && appData.budgetDay >= 0) {
                 return console.log('К сожалению, у вас уровень дохода ниже среднего');
             } else {
                 return console.log('Что то пошло не так');
@@ -65,26 +69,38 @@ let appData = {
 appData.asking();
 
 // Сумма всех обязательных расходов за месяц
-const expensesAmount = appData.getExpensesMonth();
-console.log(`Сумма всех обязательных расходов за месяц: ${expensesAmount} руб.`);
+appData.getExpensesMonth();
+console.log(`Сумма всех обязательных расходов за месяц: ${appData.expensesMonth} руб.`);
 
-// Накопления за месяц
-const accumulatedMonth = appData.getAccumulatedMonth(money, expensesAmount);
-
-// Дневной бюджет
-const budgetDay = Math.floor(accumulatedMonth/30);
-console.log(`Бюджет на день: ${budgetDay} руб.`);
+appData.getBudget();
 
 // Cрок достижения цели в месяцах
-const res = appData.getTargetMonth();
-
-if(res < 0) {
+if(appData.getTargetMonth() < 0) {
     console.log('Цель не будет достигнута');
 } else {
-    console.log(`Цель будет достигнута через: ${res} мес.`);
+    console.log(`Цель будет достигнута через: ${appData.getTargetMonth()} мес.`);
 }
-
-console.log(appData.expenses);
 
 // Уровень дохода
 appData.getStatusIncome();
+
+// Выведем в консоль все данные
+console.log('Наша программа включает в себя данные:');
+for (let key in appData) {
+    
+    if ( (typeof appData[key] != 'function') && (typeof appData[key] != 'object') ) {
+        console.log(`${key}: ${appData[key]}`);
+    }
+
+    if ( typeof appData[key] === "object" ) {
+
+        if ( Boolean(Object.keys(appData[key]).length) === true ) {
+            console.log(`${key}: ${appData[key]}`);
+        }
+
+        if ( Boolean(Object.keys(appData[key]).length) === false ) {
+            console.log(`${key}: Пустой обьект`);
+        }
+    }
+}
+
