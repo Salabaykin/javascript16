ffc0191d3e4ece0e3fa3d6a214f4188e2073be62
Evgeny <salabaikin@mail.ru>
2020-12-01 14:31:42 +0300

    lesson 7

--- lesson-8/js/main.js
+++ lesson-8/js/main.js
@@ -8,7 +8,7 @@ function isNumber(num){
 let money, 
     start = function() {
         do {
-            money = prompt('Ваш месячный доход?');
+            money = prompt('Ваш месячный доход?', 100000);
         } while(!isNumber(money));
     };
 
@@ -16,27 +16,50 @@ start();
 
 let appData = {
         income: {},
-        addIncome: {},
+        addIncome: [],
         expenses: {},
         addExpenses: [],
         deposit: false,
+        percentDeposit: 0,
+        moneyDeposit: 0,
         mission: 100000,
         period: 6,
-        budget: money,
+        budget: +money,
         budgetDay: 0,
         budgetMonth: 0,
         expensesMonth: 0,
         asking: function() {
+
+            if (confirm('Есть ли у вас дополнительный заработок ?')) {
+
+                let itemIncome = prompt('Какой у вас дополнительный заработок?', 'Таксую');
+                while (isNumber(itemIncome) || itemIncome === '' || itemIncome === null || itemIncome.trim() === '') {
+                    itemIncome = prompt('Какой у вас дополнительный заработок?', 'Фриланс');
+                }
+
+                let cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
+                while (isNaN(cashIncome) || cashIncome === '' || cashIncome === null || cashIncome.trim() === '') {
+                    cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
+                }
+
+                appData.income[itemIncome] = cashIncome;
+            }
+
             let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+            while (isNumber(itemIncome) || itemIncome === '' || itemIncome === null || itemIncome.trim() === '') {
+                addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+            }
+
             appData.addExpenses = addExpenses.toLowerCase().split(',');
             appData.deposit = confirm('Есть ли у вас депозит в банке?');
 
             let question, answer;
 
             for(let i = 0; i < 2; i++) {
-                question = prompt('Введите обязательную статью расходов?');
+                question = prompt('Введите обязательную статью расходов?', 'Aaa');
                 do {
-                    answer = prompt('Во сколько это обойдется?');
+                    answer = prompt('Во сколько это обойдется?', 200);
                 } while (!isNumber(answer)) 
                 appData.expenses[question] = +answer;
             }
@@ -63,16 +86,32 @@ let appData = {
             } else {
                 return console.log('Что то пошло не так');
             }
+        },
+        getInfoDeposit: function() {
+            if (appData.deposit) {
+
+                appData.percentDeposit = prompt('Какой годовой процент?', '10');
+                while (isNumber(appData.percentDeposit) || appData.percentDeposit === '' || appData.percentDeposit === null || appData.percentDeposit.trim() === '') {
+                    appData.percentDeposit = prompt('Какой годовой процент?', '10');
+                }
+                appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);  
+                while (isNaN(appData.moneyDeposit) || appData.moneyDeposit === '' || appData.moneyDeposit === null || appData.moneyDeposit.trim() === '') {
+                    appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+                }
+
+            }
+        },
+        calcSaveMoney: function() {
+            appData.budgetMonth * appData.period;
         }
 }
 
 appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
 
 // Сумма всех обязательных расходов за месяц
-appData.getExpensesMonth();
 console.log(`Сумма всех обязательных расходов за месяц: ${appData.expensesMonth} руб.`);
-
-appData.getBudget();
 
 // Cрок достижения цели в месяцах
 if(appData.getTargetMonth() < 0) {
@@ -87,7 +126,6 @@ appData.getStatusIncome();
 // Выведем в консоль все данные
 console.log('Наша программа включает в себя данные:');
 for (let key in appData) {
-    
     if ( (typeof appData[key] != 'function') && (typeof appData[key] != 'object') ) {
         console.log(`${key}: ${appData[key]}`);
     }
