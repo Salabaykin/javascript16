3986fca7e749fccfabb13c2a5517cc735b2750da
Evgeny <salabaikin@mail.ru>
2020-12-24 14:01:34 +0300

    Add lesson 22

--- lesson-22/js/script.js
+++ lesson-22/js/script.js
@@ -6,20 +6,25 @@ class Todo {
         this.input = document.querySelector(input);
         this.todoList = document.querySelector(todoList);
         this.todoCompleted = document.querySelector(todoCompleted);
-        this.todoData = new Map();
+        this.todoData = new Map(JSON.parse(localStorage.getItem('toDolist')));
     }
 
     render() {
+        this.todoCompleted.textContent = '';
+        this.todoList.textContent = '';
         this.todoData.forEach(this.createItem);
+        this.addToStorage();
     }
 
     addToStorage() {
-        localStorage.setItem('todolist', [...this.todoData]);
+        localStorage.setItem('toDolist', JSON.stringify([...this.todoData]));
     }
 
     createItem = (todo) => {
         const li = document.createElement('li');
         li.classList.add('todo-item');
+        li.key = key;
+        
         li.insertAdjacentHTML('beforeend', `
             <span class="text-todo">${todo.value}</span>
             <div class="todo-buttons">
@@ -52,8 +57,21 @@ class Todo {
         return (~~(Math.random()*1e8)).toString(16);
     }
 
+    deleteItem() {
+        // По ключи найти элемент и удалить 
+    }
+
+    completedItem() {
+        // Перебрать элементы 
+    }
+
+    handler() {
+        // Делегирование
+    }
+
     init() {
         this.form.addEventListener('submit', this.addTodo.bind(this));
+        this.render();
     }
 }
 
